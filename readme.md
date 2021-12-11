447d758fae71a660cea0f34c2d2143f9cb25b3f2
ppande574 <ppande0221@gmail.com>
2021-12-11 00:14:58 -0600

    Updated file parsing, readme, and data file.

--- readme.md
+++ readme.md
@@ -7,5 +7,6 @@ ppande0221@gmail.com
 This repository is hosted at: 
 https://github.com/ppande574/Currency-Converter
 
-
+This application is currently being built to support a text-based user interface. 
+After that is implemented, GUI functionality will replace it. 
 
--- src/currency_converter/FileParser.java
+++ src/currency_converter/FileParser.java
@@ -1,12 +1,59 @@
 package currency_converter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 /**
- * 
  * @author Pratik Pande
+ * 
  * This class has the functionality to read from a CSV file and store the data it contains into a HashMap. 
- *
+ * 
  */
 
 public class FileParser {
+	
 
+	public static String parseFile() {
+				
+		HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>(); 
+		
+		File f = new File("Data/topCurrencies.csv"); 
+		Scanner sc; 
+		
+		try {
+			sc = new Scanner(f);
+		} catch (FileNotFoundException fnfe) {
+			throw new RuntimeException(fnfe); 
+		} 
+		
+		sc.nextLine(); 
+		while(sc.hasNext()) {
+			String line = sc.nextLine(); 
+			if(!line.trim().isEmpty()) {
+				String tokens[] = line.split(","); 
+				String num = tokens[0]; 
+				String currencyTag = tokens[1]; 
+				
+				int currencyRank = Integer.parseInt(currencyTag); 
+				
+				// Storing parsed data in HashMap: 
+				
+				currencyCodes.put(currencyRank, currencyTag); 
+				
+			}
+		}
+		
+		sc.close();
+		
+		return null;
+		
+		
+		
+	}
+	
+
+	
 }
+	
