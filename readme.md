49557b3fa4f33c5ecdcd6a8fab70bcef52366e14
ppande574 <ppande0221@gmail.com>
2021-12-10 23:46:01 -0600

    Initial commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Currency Converter
+## Java-Based Application
+
+Pratik Pande-
+ppande0221@gmail.com
+
+This repository is hosted at: 
+https://github.com/ppande574/Currency-Converter
+
+
+
---
+++ src/currency_converter/FileParser.java
@@ -0,0 +1,12 @@
+package currency_converter;
+
+/**
+ * 
+ * @author Pratik Pande
+ * This class has the functionality to read from a CSV file and store the data it contains into a HashMap. 
+ *
+ */
+
+public class FileParser {
+
+}
